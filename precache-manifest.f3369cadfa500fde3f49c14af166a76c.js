4f49a55e07e1d24e3a95cc6a3f11c30ead2a2192
Josh LaRochelle <joshua.r.larochelle@gmail.com>
2019-01-06 15:50:25 -0500

    Updates

---
+++ precache-manifest.f3369cadfa500fde3f49c14af166a76c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e9a8c2877b153a24d9d",
+    "url": "/Tic-Tac-React/static/js/runtime~main.2e9a8c28.js"
+  },
+  {
+    "revision": "df8b53b99d3194675820",
+    "url": "/Tic-Tac-React/static/js/main.df8b53b9.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/Tic-Tac-React/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "df8b53b99d3194675820",
+    "url": "/Tic-Tac-React/static/css/main.56ff84dc.chunk.css"
+  },
+  {
+    "revision": "f87606b9a8fbd15a62782ebe7eb3298d",
+    "url": "/Tic-Tac-React/index.html"
+  }
+];
